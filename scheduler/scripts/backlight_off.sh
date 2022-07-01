215f0766319eb6b7faf818d71efbc583b546af5e
bkcberry <bryan.castleberry@gmail.com>
2022-07-01 14:29:13 -0500

    Update backlight_off.sh

--- scheduler/scripts/backlight_off.sh
+++ scheduler/scripts/backlight_off.sh
@@ -3,10 +3,4 @@
 export PATH="$PATH:/opt/vc/bin/"
 export LD_LIBRARY_PATH="/opt/vc/lib/"
 
-output=$(tvservice -s)
-
-if [[ $output =~ "0x400000 [LCD]" ]]; then
-  echo -n 1 > /sys/class/backlight/rpi_backlight/bl_power
-else
-  echo standby 0 | cec-client -d 1 -s
-fi
+echo standby 0 | cec-client -d 1 -s
